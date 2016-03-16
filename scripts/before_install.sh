92945e45c31cdf7a212e9f1a20be6f9c3f6ef849
Gareth Fleming <d3z@joocy.com>
2016-03-16 21:52:57 +0000

    Once more with feeling

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,4 +2,4 @@
 set -e # halt script on error
 
 echo Before Install...
-curl https://maker.ifttt.com/trigger/build_started/with/dU3hw4HtQ_PPckQTtijU1Q
+curl https://maker.ifttt.com/trigger/build_started/with/key/dU3hw4HtQ_PPckQTtijU1Q
